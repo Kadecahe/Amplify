e5711f609ebd6c983513bc3df352bcd921655df6
Kadecahe <kadecahe@gmail.com>
2021-03-02 23:38:44 -0500

    Drag and Reorder Podcasts

--- src/components/PodcastList.js
+++ src/components/PodcastList.js
@@ -1,36 +1,91 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlayCircle, faPauseCircle} from '@fortawesome/free-solid-svg-icons'
+import React, {useState} from 'react';
+import { Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-const PodcastList = ({allPodcasts, playPodcast, pausePodcast, audio}) => {
+const PodcastList = ({
+  allPodcasts,
+  playPodcast,
+  pausePodcast,
+  audio,
+  localPodcasts,
+  local,
+}) => {
+  let podcastList = allPodcasts;
+  const [testPodcast, setTestPodcast] = useState(podcastList)
 
-  if(!allPodcasts.length) return <h3>Loading</h3>
+  function handleOnDragEnd(result) {
+    if(!result.destination) return
+    const originalPodcasts = Array.from(testPodcast)
+    const [reorderedPodcast] = originalPodcasts.splice(result.source.index, 1)
+    originalPodcasts.splice(result.destination.index, 0, reorderedPodcast)
+    setTestPodcast(originalPodcasts)
+  }
+  if (!podcastList.length) return <h3>Loading</h3>;
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="remote">
+        {(provided) => (
+                  <div>
 
-  return (<div className='d-flex flex-column'>
-  {allPodcasts.map(podcast => (
-   <Card key={podcast.id} draggable='true'>
-     <Card.Body className="m-1 d-flex flex-row align-items-center">
-      <Card.Img style={{width: "25%"}} src={podcast.image} alt={podcast.title}/>
-     <div className="d-flex flex-column">
-     <Card.Title className="mb-1">{podcast.title}</Card.Title>
-     <Card.Text><small>{podcast.name}</small></Card.Text>
-     <Card.Text>{podcast.description}</Card.Text>
-     </div>
-     <div>
-       {!(audio === podcast.audio) ? (<FontAwesomeIcon
-      icon={faPlayCircle} onClick={() => playPodcast(podcast.audio)}/>) : <FontAwesomeIcon
-      icon={faPauseCircle} onClick={() => pausePodcast(podcast.audio)}/>}
+    <div className="d-flex flex-column" {...provided.droppableProps} ref={provided.innerRef}>
+      {testPodcast.map((podcast, index) => (
+        <Draggable key={podcast.id} draggableId={podcast.name} index={index}>
+          {(provided) => (
+            <Card
+
+              className="shadow m-3"
+              border="light"
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+            >
+              <Card.Body className="m-1 d-flex flex-row align-items-center">
+                <Card.Img
+                  style={{ width: '25%' }}
+                  src={podcast.image}
+                  alt={podcast.title}
+                />
+                <div className="d-flex flex-column">
+                  <Card.Title className="mb-1">{podcast.title}</Card.Title>
+                  <Card.Text>
+                    <small>{podcast.name}</small>
+                  </Card.Text>
+                  <Card.Text>{podcast.description}</Card.Text>
+                </div>
+                <div>
+                  {!(audio === podcast.audio) ? (
+                    <FontAwesomeIcon
+                      icon={faPlayCircle}
+                      onClick={() => playPodcast(podcast.audio)}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faPauseCircle}
+                      onClick={() => pausePodcast(podcast.audio)}
+                    />
+                  )}
+                </div>
+              </Card.Body>
+            </Card>
+          )}
+        </Draggable>
+      ))}
+      {provided.placeholder}
+    </div>
       </div>
-     </Card.Body>
-   </Card>
-))}
-</div>)
-}
+
+    )}
+    </Droppable>
+    </DragDropContext>
+  );
+};
 
 const mapStateToProps = state => ({
-  allPodcasts: state.podcasts
-})
+  allPodcasts: state.podcasts,
+  localPodcasts: [],
+});
 
-export default connect(mapStateToProps)(PodcastList)
+export default connect(mapStateToProps)(PodcastList);
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@
 export {default as About} from './About'
 export {default as Main} from './Main'
 export {default as Navbar} from './Navbar'
+export {default as DragList} from './DragList'
+export {default as PodcastList} from './PodcastList'
