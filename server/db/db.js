c83a959325a8bc59be96295452a3a7dc61c9c0e7
Kadecahe <kadecahe@gmail.com>
2021-04-09 21:14:24 -0400

    update db config

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,22 @@ exports.__esModule = true;
 exports.db = void 0;
 //Testing the import on the backend. I tried require but gave me a TS error
 var sequelize_1 = require("sequelize");
-exports.db = new sequelize_1.Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/amplify", {
-    logging: false
-});
+var config;
+if (process.env.DATABASE_URL) {
+    config = {
+        logging: false,
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    };
+}
+else {
+    config = {
+        logging: false
+    };
+}
+exports.db = new sequelize_1.Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/amplify", config);
--- server/db/db.ts
+++ server/db/db.ts
@@ -1,9 +1,24 @@
 //Testing the import on the backend. I tried require but gave me a TS error
 import { Sequelize } from 'sequelize';
+let config;
 
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
+    logging: false
+  }
+}
 export const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/amplify`,
-  {
-    logging: false,
-  }
+  config
 );
